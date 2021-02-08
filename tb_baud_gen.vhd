1cd73cb76ab0fc720a1bc60105bb33b2d5af956a
Jessica <fencl.dian@gmail.com>
2021-02-08 15:10:12 +0100

    Update testbench baud gen

--- tb_baud_gen.vhd
+++ tb_baud_gen.vhd
@@ -11,16 +11,19 @@ architecture sim of tb_baud_gen is
 component baud_gen is       -- component definuje stejne jako ta entita ktera se testuje
  port
     (
-     i_clkin  : in std_logic;
-     i_res    : in std_logic;
-     o_clkout : out std_logic
-       --clk_per_bits : out integer;
+       i_clkin     : in std_logic;
+       i_res       : in std_logic;
+       i_start_clk : in std_logic;
+       --o_cnt_done  : out std_logic;
+       o_clkout    : out std_logic
     );
 end component;
 
-    signal clk:std_logic;
-    signal reset:std_logic;
-    signal clkout:std_logic;
+    signal clk: std_logic;
+    signal reset: std_logic;
+    signal start: std_logic;
+    --signal cnt_done: std_logic;
+    signal clkout: std_logic;
     
 begin
 dut: baud_gen
@@ -28,26 +31,44 @@ dut: baud_gen
      (
       i_clkin=>clk,
       i_res=>reset,
+      i_start_clk=>start,
+      --o_cnt_done=>cnt_done,
       o_clkout=>clkout
      );
      
 process 
 begin
     reset<='0';
-    wait for 100 ns;
+    wait for 20 ns;
     reset<= '1';
+    wait;
+end process;
+
+process 
+begin
+    start<='0';
+    wait for 60 ns;
+    start<= '1';
+    wait for 60 ns;
+    start<='0';
+    wait for 60 ns;
     wait;
 end process;
 
 process 
 begin    
     clk<='0';
-    wait for 100 ns;
+    wait for 60 ns;
     clk<='1';
-    wait for 100 ns;
+    wait for 60 ns;
     clk<='0';
-    wait for 100 ns;
+    wait for 60 ns;
     clk<='1';
-    wait for 100 ns; 
+    wait for 60 ns;
+     clk<='0';
+    wait for 60 ns;
+    clk<='1';
+    wait for 60 ns;
+     
 end process;              
 end;
