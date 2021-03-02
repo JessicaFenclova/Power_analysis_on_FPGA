9be47f0c0303dd53b21093b28294a0ee6e7c9627
Jessica <fencl.dian@gmail.com>
2021-03-02 12:25:06 +0100

    Updated testbench for controll aes block

--- tb_ctrl_aes.vhd
+++ tb_ctrl_aes.vhd
@@ -14,33 +14,28 @@ architecture sim of tb_ctrl_aes is
     i_rst         : in std_logic;
     i_rd_req      : in std_logic;
     i_data        : in std_logic_vector(7 downto 0);
-    i_cmd_done    : in std_logic;
-    i_eval_result : in std_logic_vector(7 downto 0);
+    i_eval_result : in std_logic;
     i_wr_ack      : in std_logic;
-    o_rd_ack      : out std_logic; 
-    o_start_cmd   : out std_logic;
-    o_cmd_reg     : out std_logic_vector(2 downto 0);
-    o_param_reg   : out std_logic_vector(4 downto 0);
+    o_rd_ack      : out std_logic; -- do i want another signal out ready
     o_wr_req      : out std_logic;
-    o_data        : out std_logic_vector(7 downto 0)  
+    o_lsb         : out std_logic_vector(4 downto 0);
+    o_msb         : out std_logic_vector(4 downto 0);
+    o_data        : out std_logic  
     );
     
  end component;
  
-     signal clk    : std_logic;
-     signal reset    : std_logic;
-     signal rd_req   : std_logic;
-     signal data  : std_logic_vector(7 downto 0);
-     signal dataout   : std_logic_vector(7 downto 0);
-     --signal start   : std_logic;
-     signal cmd_done    :  std_logic;
-     signal ev_result :  std_logic_vector(7 downto 0);
-     signal wr_ack      :  std_logic;
-     signal rd_ack      :  std_logic; 
-     signal start_cmd   : std_logic;
-     signal cmd_reg     : std_logic_vector(2 downto 0);
-     signal param_reg   :  std_logic_vector(4 downto 0);
-     signal wr_req      :  std_logic;
+     signal clk       : std_logic;
+     signal reset     : std_logic;
+     signal rd_req    : std_logic;
+     signal data      : std_logic_vector(7 downto 0);
+     signal dataout   : std_logic;
+     signal ev_result :  std_logic;
+     signal wr_ack    :  std_logic;
+     signal rd_ack    :  std_logic; 
+     signal wr_req    :  std_logic;
+     signal lsb       : std_logic_vector(4 downto 0);
+     signal msb       : std_logic_vector(4 downto 0);
      signal e : std_logic;
 
     
@@ -53,14 +48,12 @@ architecture sim of tb_ctrl_aes is
       i_rst=>reset,
       i_rd_req=>rd_req,
       i_data=>data,
-      i_cmd_done=>cmd_done,
       i_eval_result=>ev_result,
       i_wr_ack=>wr_ack,
       o_rd_ack=>rd_ack,
-      o_start_cmd=> start_cmd,
-      o_cmd_reg=> cmd_reg,
-      o_param_reg=> param_reg,
       o_wr_req=> wr_req,
+      o_lsb=>lsb,
+      o_msb=>msb,
       o_data=> dataout
                 
      );
@@ -93,35 +86,61 @@ architecture sim of tb_ctrl_aes is
       rd_req<= '1';
       wait for 100 ns;
       rd_req<='0';
-      wait;
- end process;
- 
- process 
-   begin
-      cmd_done<='0';
-      wait for 8800 ns;
-      cmd_done<= '1';
-      wait for 400 ns;
-      cmd_done<='0';
+      wait for 800 ns;
+      rd_req<= '1';
+      wait for 100 ns;
+      rd_req<='0';
+      wait for 1840 ns;
+      rd_req<= '1';
+      wait for 100 ns;
+      rd_req<='0';
+      wait for 1940 ns;
+      rd_req<= '1';
+      wait for 100 ns;
+      rd_req<='0';
       wait;
  end process; 
  
  process 
    begin
-      ev_result<="00000000";
-      wait for 3800 ns;
-      ev_result<="00000011";
-      wait for 4000 ns;
-    
+      ev_result<='0';
+      wait for 840 ns;
+      ev_result<='1';
+      wait for 100 ns;
+      ev_result<='0';
+      wait for 1820 ns;
+      ev_result<='1';
+      wait for 100 ns;
+      ev_result<='0';
+      wait for 1820 ns;
+      ev_result<='1';
+      wait for 100 ns;
+      ev_result<='0';
+      wait for 2000 ns;
+      ev_result<='1';
+      wait for 100 ns;
+      ev_result<='0';
       wait;
  end process;
  
   process 
    begin
       wr_ack<='0';
-      wait for 4600 ns;
+      wait for 940 ns;
       wr_ack<= '1';
-      wait for 600 ns;
+      wait for 100 ns;
+      wr_ack<='0';
+      wait for 1800 ns;
+      wr_ack<= '1';
+      wait for 100 ns;
+      wr_ack<='0';
+      wait for 1800 ns;
+      wr_ack<= '1';
+      wait for 100 ns;
+      wr_ack<='0';
+      wait for 2000 ns;
+      wr_ack<= '1';
+      wait for 100 ns;
       wr_ack<='0';
       wait;
  end process;  
@@ -130,10 +149,14 @@ architecture sim of tb_ctrl_aes is
    begin
  
       e <= '0';
-      data <= "00000000";   --begin
-      wait for 4840 ns;
-      data <= "11001100";   --begin
-      wait for 4840 ns;
+      data <= "00000000";   --begin , but the cmd 000 is for setting lsb
+      wait for 1000 ns;
+      data <= "11111010";   --cmd for enable sbox, the 3 bits from the end
+      wait for 2000 ns;
+      data <= "11111000";   --cmd for set lsb
+      wait for 2000 ns;
+      data <= "11111001";  -- cmd for set msb
+      wait for 2000 ns;
 
     
      e <= '1';
