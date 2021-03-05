842b398770769fc68bba5d942ab680be3f7fc4e4
Jessica <fencl.dian@gmail.com>
2021-03-05 14:05:08 +0100

    Updated testbench lfsr

--- tb_lfsr.vhd
+++ tb_lfsr.vhd
@@ -10,24 +10,23 @@ architecture sim of tb_lfsr is
 
  component lfsr is 
   port (
-        in_clk        : in std_logic;
-        in_rst        : in std_logic;
-        --i_sel_ctrl   : in std_logic_vector(1 downto 0);
-        i_lsb_en    : in std_logic;
-        i_msb_en      : in std_logic;
-        i_lsb_bits  : in std_logic_vector(4 downto 0);
-        i_msb_bits  : in std_logic_vector(4 downto 0);
+        in_clk       : in std_logic;
+        in_rst       : in std_logic;
+        i_gener_data : in std_logic;    -- from the state machine measure, -- input from the state machine for measruing that says generate data here in lfsr
+        i_lsb_en     : in std_logic;
+        i_msb_en     : in std_logic;
+        i_param_bits : in std_logic_vector(4 downto 0);
         o_gener_bits : out std_logic_vector(7 downto 0)  
     );
     
  end component;
  
-     signal clk       : std_logic;
-     signal reset     : std_logic;     
-     signal lsb       : std_logic_vector(4 downto 0);
-     signal msb       : std_logic_vector(4 downto 0);
-     signal lsb_en    : std_logic;
-     signal msb_en    : std_logic;
+     signal clk        : std_logic;
+     signal reset      : std_logic;
+     signal gener_data : std_logic;      
+     signal bits       : std_logic_vector(4 downto 0);
+     signal lsb_en     : std_logic;
+     signal msb_en     : std_logic;
      signal gener_bits : std_logic_vector(7 downto 0);
      signal e : std_logic;
 
@@ -39,8 +38,8 @@ architecture sim of tb_lfsr is
      (
       in_clk=>clk,
       in_rst=>reset,
-      i_lsb_bits=>lsb,
-      i_msb_bits=>msb,
+      i_gener_data => gener_data,
+      i_param_bits=>bits,
       i_lsb_en => lsb_en,
       i_msb_en => msb_en,
       o_gener_bits=> gener_bits
@@ -89,15 +88,13 @@ architecture sim of tb_lfsr is
       wait;
  end process;
  
-  process 
+ process 
    begin
-      msb<="00000";
-      wait for 840 ns;
-      msb<= "10001";
+      gener_data<='0';
+      wait for 2000 ns;
+      gener_data<= '1';
       wait for 200 ns;
-      msb<="00000";
-      wait for 1800 ns;
-      
+      gener_data<='0';
       wait;
  end process;  
  
@@ -105,11 +102,15 @@ architecture sim of tb_lfsr is
    begin
  
       e <= '0';
-      lsb<="00000";
+      bits<="00000";    --for the lsb
       wait for 180 ns;
-      lsb<= "01110";
+      bits<= "01110";
       wait for 200 ns;
-      lsb<="00000";
+      bits<="00000";  -- for the msb
+      wait for 840 ns;
+      bits<= "10001";
+      wait for 200 ns;
+      bits<="00000";
       wait for 1800 ns;
 
     
