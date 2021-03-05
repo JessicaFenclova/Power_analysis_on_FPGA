27581211f5e1483ac659214bb28d6550543992f5
Jessica <fencl.dian@gmail.com>
2021-03-05 14:04:42 +0100

    Updated lfsr

--- lfsr.vhd
+++ lfsr.vhd
@@ -5,14 +5,16 @@ use ieee.numeric_std.all;
 
  entity lfsr is
   port (
-        in_clk        : in std_logic;
-        in_rst        : in std_logic;
-        --i_sel_ctrl   : in std_logic_vector(1 downto 0);
-        i_lsb_en    : in std_logic;
-        i_msb_en      : in std_logic;
-        i_lsb_bits  : in std_logic_vector(4 downto 0);
-        i_msb_bits  : in std_logic_vector(4 downto 0);
+        in_clk       : in std_logic;
+        in_rst       : in std_logic;
+        i_gener_data : in std_logic;    -- from the state machine measure, -- input from the state machine for measruing that says generate data here in lfsr
+        i_lsb_en     : in std_logic;
+        i_msb_en     : in std_logic;
+        i_param_bits : in std_logic_vector(4 downto 0);
+        --i_lsb_bits  : in std_logic_vector(4 downto 0);   -- will be only one input with the 4 bits from the param reg for lsb or msb
+        --i_msb_bits  : in std_logic_vector(4 downto 0);
         o_gener_bits : out std_logic_vector(7 downto 0)
+        
        );
   end lfsr;
   
@@ -34,25 +36,31 @@ use ieee.numeric_std.all;
            o_gener_bits<="00000000";
            out_shift_xor<='0';
         elsif in_clk'event and (in_clk='1') then
-            --out_lfsr  -- using for to go bit by bit and xor them, depending on the polynom, chosen bits
-            if(i_lsb_en='1') then
-               bits_reg<= i_lsb_bits(3 downto 0);
+            
+            if (i_gener_data='0') and (i_lsb_en='1') then
+               bits_reg<= i_param_bits(3 downto 0);
                in_lfsr(3 downto 0)<= bits_reg;
-            elsif (i_msb_en='1') then
-               bits_reg<= i_msb_bits(3 downto 0);
+            elsif (i_gener_data='0') and (i_msb_en='1') then
+               bits_reg<= i_param_bits(3 downto 0);
                in_lfsr(7 downto 4)<=bits_reg;
-            else
-               in_lfsr<=out_lfsr;
-            
+            elsif (i_gener_data='1') then
+               o_gener_bits <= in_lfsr; -- in_lfsr<= out_lfsr, in_lfsr<=in_lfsr, o_gener_bits <= in_lfsr            
             end if;
-            out_shift_xor<=((in_lfsr(0)) XOR (in_lfsr(2)) XOR (in_lfsr(3)) XOR (in_lfsr(4)));
-            out_lfsr(0)<=out_shift_xor;
-            out_lfsr(7 downto 1)<=in_lfsr(6 downto 0);
             
-            --in_lfsr<= in_lfsr(7 downto 1) & out_shift_xor;
-            o_gener_bits<= in_lfsr;
+            --out_shift_xor<=((in_lfsr(0)) XOR (in_lfsr(2)) XOR (in_lfsr(3)) XOR (in_lfsr(4)));
+            --out_lfsr(0)<=out_shift_xor;
+            --out_lfsr(7 downto 1)<=in_lfsr(6 downto 0);
+            --in_lfsr<=out_lfsr;
+
+            --o_gener_bits<= out_lfsr;
         end if;
   end process p_lfsr;
+  
+  out_shift_xor<=((in_lfsr(0)) XOR (in_lfsr(2)) XOR (in_lfsr(3)) XOR (in_lfsr(4)));
+  out_lfsr(0)<=out_shift_xor;
+  out_lfsr(7 downto 1)<=in_lfsr(6 downto 0);
+  in_lfsr<=out_lfsr;
+  o_gener_bits<= out_lfsr;
 
 
  end architecture rtl;
