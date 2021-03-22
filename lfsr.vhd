b6db9e0e2cdb94266aa262752e860adfe1516985
Jessica <fencl.dian@gmail.com>
2021-03-22 02:56:11 +0100

    LFSR changed but not functioning

--- lfsr.vhd
+++ lfsr.vhd
@@ -21,8 +21,8 @@ use ieee.numeric_std.all;
   architecture rtl of lfsr is
   
      signal bits_reg      : std_logic_vector(3 downto 0);
-     signal out_lfsr      : std_logic_vector(7 downto 0);
-     signal in_lfsr       : std_logic_vector(7 downto 0);
+     signal out_lfsr      : std_logic_vector(7 downto 0):="11111111";
+     signal in_lfsr       : std_logic_vector(7 downto 0):="11111111";
      --signal out_shift_xor : std_logic; 
      
      
@@ -31,14 +31,14 @@ use ieee.numeric_std.all;
    p_lfsr : process(in_clk, in_rst)
     begin
         if in_rst = '0' then
-           bits_reg<="0000";
-           in_lfsr <="11111111";
-           out_lfsr <="11111111";
+           --bits_reg<="0000";
+           --in_lfsr <="11111111";
+           --out_lfsr <="11111111";
            --o_gener_bits<="00000000";
            --out_shift_xor<='0';                          
         elsif in_clk'event and (in_clk='1') then
-             o_gener_bits<= out_lfsr;
-             --out_lfsr<=in_lfsr;
+             --o_gener_bits<= out_lfsr;
+             out_lfsr<=in_lfsr;
             --if (i_gener_data='1') then
                --in_lfsr<= in_lfsr; -- in_lfsr<= out_lfsr, in_lfsr<=in_lfsr, o_gener_bits <= in_lfsr
                --in_lfsr<=out_lfsr;
@@ -68,31 +68,31 @@ use ieee.numeric_std.all;
   p_lfsr_2 : process(i_gener_data, i_lsb_en, i_msb_en)
     begin
        if (i_gener_data='1') then
-          out_lfsr(0)<=((in_lfsr(0)) XOR (in_lfsr(2)) XOR (in_lfsr(3)) XOR (in_lfsr(4)));
-          out_lfsr(7 downto 1)<=in_lfsr(6 downto 0);
-          in_lfsr<=out_lfsr;         
+          in_lfsr(0)<=((out_lfsr(0)) XOR (out_lfsr(2)) XOR (out_lfsr(3)) XOR (out_lfsr(4))); -- switch out with in
+          in_lfsr(7 downto 1)<=out_lfsr(6 downto 0);
+          --in_lfsr<=out_lfsr;         
        --elsif (i_gener_data='0') then
             --if (i_lsb_en='1') then 
-               bits_reg<= i_param_bits(3 downto 0);
-               in_lfsr(3 downto 0)<= bits_reg;
+               --bits_reg<= i_param_bits(3 downto 0);
+               --in_lfsr(3 downto 0)<= bits_reg;
             --elsif (i_msb_en='1') then 
-               bits_reg<= i_param_bits(3 downto 0);
-               in_lfsr(7 downto 4)<=bits_reg;
+               --bits_reg<= i_param_bits(3 downto 0);
+               --in_lfsr(7 downto 4)<=bits_reg;
             --end if;       
        end if;
        if (i_lsb_en='1') then 
                bits_reg<= i_param_bits(3 downto 0);
                in_lfsr(3 downto 0)<= bits_reg;
-               out_lfsr<=in_lfsr;
+               --out_lfsr<=in_lfsr;
        elsif (i_msb_en='1') then 
                bits_reg<= i_param_bits(3 downto 0);
                in_lfsr(7 downto 4)<=bits_reg;
-               out_lfsr<=in_lfsr;
+               --out_lfsr<=in_lfsr;
        end if;
   end process p_lfsr_2;
   
- 
-  --o_gener_bits<= out_lfsr;
+  
+  o_gener_bits<= out_lfsr;
 
 
  end architecture rtl;
