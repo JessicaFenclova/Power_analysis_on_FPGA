5d4a1203bc6dfc323dff69337a701d6432a1edb6
Jessica <fencl.dian@gmail.com>
2021-05-26 22:09:29 +0200

    Testbench for top of the ctrl and uart entities.

--- tb_top_ctrl_and_uart.vhd
+++ tb_top_ctrl_and_uart.vhd
@@ -16,6 +16,7 @@ architecture sim of tb_top_ctrl_and_uart is
          i_data_sbox : in std_logic_vector(7 downto 0);
          o_trigger   : out std_logic;
          o_bits_sbox : out std_logic_vector(7 downto 0);
+         o_test_bit  : out std_logic;
          o_data      : out std_logic           
   
        );
@@ -29,6 +30,7 @@ architecture sim of tb_top_ctrl_and_uart is
      signal datain    : std_logic;
      signal datasbox  : std_logic_vector(7 downto 0);
      signal gener_bits: std_logic_vector(7 downto 0);
+     signal test      : std_logic;
      signal e : std_logic;
 
     
@@ -43,6 +45,7 @@ architecture sim of tb_top_ctrl_and_uart is
       i_data_sbox  => datasbox,  
       o_trigger   => trig,
       o_bits_sbox => gener_bits,
+      o_test_bit => test,
       o_data  => data_out
                 
      );
@@ -50,7 +53,11 @@ architecture sim of tb_top_ctrl_and_uart is
  process 
    begin
       reset<='0';
-      wait for 10 ns;
+      wait for 20 ns;
+      reset<= '1';
+      wait for 30 ns;
+      reset<='0';
+      wait for 30 ns;
       reset<= '1';
       wait;
  end process;
@@ -72,15 +79,21 @@ architecture sim of tb_top_ctrl_and_uart is
   process 
    begin
       datasbox <= "00000000";  
-      wait for 1100 ns;
+      wait for 401000 ns;
       datasbox <= "11110000";  
-      wait for 2120 ns;
+      wait for 81200 ns;
       datasbox <= "11111111";   
-      wait for 2100 ns;
+      wait for 81500 ns;
       datasbox <= "00110011"; 
-      wait for 2100 ns;
+      wait for 61000 ns;
       datasbox <= "10000001";  
-      wait for 2100 ns;
+      wait for 61000 ns;
+      datasbox <= "11110000";  
+      wait for 61200 ns;
+      datasbox <= "11111111";
+      wait for 681000 ns;
+      datasbox <= "00000000";  
+      
       wait;
  end process; 
  
@@ -88,8 +101,8 @@ architecture sim of tb_top_ctrl_and_uart is
    begin
       e <= '0';
       
-      datain <= '1';   --begin
-      wait for 18620 ns;
+      --datain <= '1';   --begin
+      --wait for 18620 ns;
       datain <= '0';   --0
       wait for 17400 ns; 
       datain <= '1';    --1
@@ -104,29 +117,89 @@ architecture sim of tb_top_ctrl_and_uart is
       wait for 17400 ns;
       datain <= '1';     --6
       wait for 17400 ns;
+      datain <= '1';     --7
+      wait for 17400 ns;
+      datain <= '1';     --8
+      wait for 17400 ns;
+      datain <= '1';     --9
+      wait for 52400 ns;
+      datain <= '0';   --0   -----------------
+      wait for 17400 ns; 
+      datain <= '1';    --1
+      wait for 17400 ns;
+      datain <= '0';    --2
+      wait for 17400 ns;
+      datain <= '1';    --3
+      wait for 17400 ns;
+      datain <= '0';    --4
+      wait for 17400 ns;
+      datain <= '1';    --5
+      wait for 17400 ns;
+      datain <= '0';     --6
+      wait for 17400 ns;
+      datain <= '1';     --7
+      wait for 17400 ns;
+      datain <= '0';     --8
+      wait for 17400 ns;
+      datain <= '1';     --9
+      wait for 52400 ns;
+      datain <= '0';   --0 ============
+      wait for 17400 ns; 
+      datain <= '1';    --1
+      wait for 17400 ns;
+      datain <= '1';    --2
+      wait for 17400 ns;
+      datain <= '1';    --3
+      wait for 17400 ns;
+      datain <= '1';    --4
+      wait for 17400 ns;
+      datain <= '0';    --5
+      wait for 17400 ns;
+      datain <= '0';     --6
+      wait for 17400 ns;
       datain <= '0';     --7
       wait for 17400 ns;
       datain <= '0';     --8
       wait for 17400 ns;
       datain <= '1';     --9
       wait for 52400 ns;
-      datain <= '0';   --0
+      datain <= '0';   --0 ´´´´´´´´´´´´´´´´´´´´
       wait for 17400 ns; 
       datain <= '1';    --1
       wait for 17400 ns;
-      datain <= '1';    --2
+      datain <= '0';    --2
       wait for 17400 ns;
-      datain <= '0';    --3
+      datain <= '1';    --3
       wait for 17400 ns;
       datain <= '0';    --4
       wait for 17400 ns;
       datain <= '1';    --5
       wait for 17400 ns;
-      datain <= '1';     --6
+      datain <= '0';     --6
       wait for 17400 ns;
-      datain <= '0';     --7
+      datain <= '1';     --7
       wait for 17400 ns;
-      datain <= '0';     --8
+      datain <= '1';     --8
+      wait for 17400 ns;
+      datain <= '1';     --9 
+      wait for 52400 ns;
+      datain <= '0';   --0 ´========================
+      wait for 17400 ns; 
+      datain <= '1';    --1
+      wait for 17400 ns;
+      datain <= '1';    --2
+      wait for 17400 ns;
+      datain <= '1';    --3
+      wait for 17400 ns;
+      datain <= '1';    --4
+      wait for 17400 ns;
+      datain <= '1';    --5
+      wait for 17400 ns;
+      datain <= '0';     --6
+      wait for 17400 ns;
+      datain <= '1';     --7
+      wait for 17400 ns;
+      datain <= '1';     --8
       wait for 17400 ns;
       datain <= '1';     --9 
       wait for 6 ms; 
